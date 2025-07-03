dc542536b4f47d8fb9448d957c1f0b9b31b4c849
Avnish Kumar <126278725+AvnishKr01@users.noreply.github.com>
2025-07-03 23:49:25 +0530

    first commit

--- Server/server.js
+++ Server/server.js
@@ -18,7 +18,6 @@ connectCloudinary();
 
 var corsOptions = {
     origin:['http://localhost:5173', 'http://localhost:5174'],
-    origin:['http://localhost:5173', 'http://localhost:5174'],
     methods:'GET, POST, DELETE, PATCH, HEAD',
     Credential:true,
 }
